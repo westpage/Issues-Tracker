139feb88cb997d8966e2ab6e6967b4881cb6f72c
westpage <westpage8@gmail.com>
2024-05-22 01:54:03 -0700

    Feat: Added functionalities

--- public/app.js
+++ public/app.js
@@ -2,36 +2,39 @@
 
 // LET'S CREATE OUR MODAL FUNCTIONALITY
 // get modal variables
-var modalOverlay = document.getElementById("modalOverlay");
-var closeForm = document.getElementById("closeForm");
+var closeModalOverlay = document.getElementById("modalOverlay");
+var closeModalForm = document.getElementById("modalForm");
 var successMsg = document.getElementById("successMsg");
 var openMsg = false;
 
-var addIssueBtn = document.getElementById("addIssueBtn");
-addIssueBtn.addEventListener("click", function (e) {
-  e.preventDefault();
+// Modal trigger button
+var addIssueBtn = document
+  .getElementById("addIssueBtn")
+  .addEventListener("click", function (e) {
+    e.preventDefault();
 
-  if (e.target.id == "addIssueBtn") {
-    if (openMsg == false) {
-      if (modalOverlay.classList.contains("hidden")) {
-        openMsg = true;
-        modalOverlay.classList.remove("hidden");
+    if (e.target.id == "addIssueBtn") {
+      if (openMsg == false) {
+        if (modalOverlay.classList.contains("hidden")) {
+          openMsg = true;
+          modalOverlay.classList.remove("hidden");
+        } else {
+          modalOverlay.classList.add("hidden");
+          openMsg = false;
+        }
       } else {
-        modalOverlay.classList.add("hidden");
-        openMsg = false;
-      }
-    } else {
-      if (modalOverlay.classList.contains("hidden")) {
-        openMsg = true;
-        modalOverlay.classList.remove("hidden");
-      } else {
-        modalOverlay.classList.add("hidden");
+        if (modalOverlay.classList.contains("hidden")) {
+          openMsg = true;
+          modalOverlay.classList.remove("hidden");
+        } else {
+          modalOverlay.classList.add("hidden");
+        }
       }
     }
-  }
-});
+  });
 
-modalOverlay.addEventListener("click", function (e) {
+// Close Modal Overlay
+closeModalOverlay.addEventListener("click", function (e) {
   e.preventDefault();
 
   if (e.target.id == "modalOverlay") {
@@ -39,13 +42,15 @@ modalOverlay.addEventListener("click", function (e) {
   }
 });
 
-closeForm.addEventListener("click", function (e) {
+// Close Modal Form
+closeModalForm.addEventListener("click", function (e) {
   e.preventDefault();
   if (e.target.id == "closeForm") {
     closeModalFunc();
   }
 });
 
+// Close Modal Function
 function closeModalFunc() {
   if (openMsg) {
     modalOverlay.classList.add("hidden");
@@ -55,26 +60,32 @@ function closeModalFunc() {
   }
 }
 
-// Form Components
+/**********end of modal functionalities***************** */
 
+/**********LET'S CREATE OUR FORM FUNCTIONALITIES**************************** */
+// get form variables
 var formAddBtn = document.getElementById("formAddBtn");
-formAddBtn.addEventListener("click", createNewIssue);
-
 let emptyContainer = document.getElementById("emptyContainer");
 
+// Add Event Handler
+formAddBtn.addEventListener("click", createNewIssue);
+
+// CreateNewIssue function
 function createNewIssue(e) {
-  // Prevent Default Behavior
   e.preventDefault();
 
-  // assign variables to all the form inputs elements
+  //get form input variables and their values
   var issueDesc = document.getElementById("issueDescriptionInput").value;
   var issueSeverity = document.getElementById("issueSeverityInput").value;
   var issueAssignedTo = document.getElementById("issueAssignedToInput").value;
   var issueId = Math.floor(Math.random() * 800 + 1);
   var issueStatus = "Open";
 
+  // Animate button on submit
   formAddBtn.classList.add("animate", "animate-pulse");
   formAddBtn.innerText = "Please wait...";
+
+  // show success
   setTimeout(function () {
     setTimeout(function () {
       successMsg.classList.remove("hidden");
@@ -88,9 +99,8 @@ function createNewIssue(e) {
       }, 2500);
     }, 2000);
   }, 1000);
-  //  formAddBtn.classList.add("animate animate-bounce");
 
-  //  Create an object to hold the properties of our new created issue
+  //  Create an object to store the values of our new created issue
   var ourIssueObject = {
     id: issueId,
     description: issueDesc,
@@ -99,52 +109,63 @@ function createNewIssue(e) {
     status: issueStatus,
   };
 
-  // Lets check to see if we already have issues from ourLocalStorage
+  // Lets check to see if we already have issues from ourLocalStorage, so we could add to what we already have..
   if (localStorage.getItem("issues") == null) {
     // Initialize an empty Array
+    var issuesArray = [];
+
     emptyContainer.innerHTML = '<h1 class="">No Issues Yet</h1>';
 
-    var issuesArray = [];
+    // push ourIssueObject into the issueArray
     issuesArray.push(ourIssueObject);
 
-    // Then let's add it to ourLocalStorage
+    // Then let's add our new Issue to ourLocalStorage
     localStorage.setItem("issues", JSON.stringify(issuesArray));
   } else {
+    // If we have issues already then
     // get what ever issues we have on our localStorage
     let ourLocalStorage = JSON.parse(localStorage.getItem("issues"));
+
+    //Push ourIssueObject into what we got from ourLocalStorage
     ourLocalStorage.push(ourIssueObject);
+
+    // Set Updated Issues Array to localStorage
     localStorage.setItem("issues", JSON.stringify(ourLocalStorage));
   }
 
+  // Reset the form data
   document.getElementById("issueInputForm").reset();
 
-  // Refetch From localStorage
+  //Empty our Issues Container
   emptyContainer.innerHTML = "";
 
+  // Now Refetch from our localStorage
   fetchIssueList();
 }
 
-// STep 2 is to Create a function that will check if we have any already existing issues on our LocalStorage, and if yes then we want display the existing issues on the issueList Container of our page
+/*LET'S CREATE A FUNCTION TO FETCH AND UPDATE OUR CONTENTS */
 
 function fetchIssueList() {
   // Get issues from our localStorage
-
   let ourLocalStorage = JSON.parse(localStorage.getItem("issues"));
 
+  // check if nothing exist inside ourLocalStorage
   if (!ourLocalStorage) {
     emptyContainer.innerHTML =
       '<h1 class="font-mono text-center">No Issues Yet <br/> <a href="#issuesWrapper" class="mt-2 text-base text-blue-600 font-bold">Add New Issues</a> </h1>';
   } else {
-    // Get the issuesContainer Element to house all our issues
+    // If we have issues from ourLocalStorage
+    // Get the issuesContainer to house all our issues
     let issuesContainer = document.getElementById("issuesContainer");
     // get the issuesContainer empty it and fill it with the issues from the ourLocalStorage
 
-    emptyContainer.classList.add("hidden");
-
-    //   Empty the issuesContainer for now
+    // Empty the issuesContainer for now
     issuesContainer.innerHTML = "";
 
-    // Run through the issues we got from ourLocalStorage and give eeach on its body and add it to the issuesContainer
+    // Hide EmptyContainer Message
+    emptyContainer.classList.add("hidden");
+
+    // Run through the issues we got from ourLocalStorage and give each one its body frame and add it to the issuesContainer
     for (var i = 0; i < ourLocalStorage.length; i++) {
       // Get issues Object Properties and assign them to variables
       let id = ourLocalStorage[i].id;
@@ -154,9 +175,8 @@ function fetchIssueList() {
       let status = ourLocalStorage[i].status;
 
       //  Now add each issues to our issuesContainer by first clothing each issue element in its case element
-
       issuesContainer.innerHTML +=
-        ' <div class="w-full sm:max-w-[550px] md:max-w-[340px] lg:max-w-[420px] mt-6 bg-white shadow-sm  hover:shadow-lg rounded-md">' +
+        ' <div class="w-full sm:max-w-[550px] md:max-w-[340px] lg:max-w-[420px] mt-6 bg-white shadow-sm  hover:shadow-xl rounded-md">' +
         '<div class="w-full flex flex-col rounded-lg border-dashed border-b-2 border-stone-200 py-4 px-4">' +
         '      <div class="w-full flex items-center shadow-inner justify-between">' +
         ' <p class="w-full bg-white flex items-center justify-start py-2 px-2  text-xs text-stone-700 font-bold">Issue ID: ' +
@@ -165,7 +185,7 @@ function fetchIssueList() {
         "</span>" +
         "    </p>" +
         '      <p class="w-full bg-white flex items-center justify-end py-2 px-2  text-xs text-stone-700 font-bold">Status: ' +
-        ' <span class="bg-gray-50 shadow-inner text-green-500  font-semibold rounded-xl text-xs ml-2 px-2 py-1">' +
+        ' <span class="bg-gray-50 text-xs text-gray-500 ml-2 rounded-md shadow-sm font-semibold px-1 py-1">' +
         status +
         "  </span>" +
         "</p>" +
@@ -187,37 +207,55 @@ function fetchIssueList() {
         "  </p>" +
         "     </div>" +
         '<div class="w-full px-6 py-4 flex items-center justify-start gap-4">' +
-        '<a href=""  onclick="setStatusClosed(\'' +id+ '\')" class="text-xs text-blue-500 font-semibold rounded-md shadow-sm hover:shadow-xl px-2 py-1">Close </a>' +
-        '<a href=""  onclick="deleteStatus(\'' +id+ '\')" class="text-xs text-red-500 bg-gray-50 font-semibold rounded-md shadow-sm hover:shadow-xl px-2 py-1">Delete </a> ' 
-        +"</div>";
+        '<a href=""  onclick="setStatusClosed(\'' +
+        id +
+        '\')" class="flex items-center gap-1 text-xs text-blue-400 font-semibold rounded-md bg-gray-50 hover:bg-blue-50 shadow-inner hover:shadow-sm px-2 py-1"> ' +
+        `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M16.5 10.5V6.75a4.5 4.5 0 1 0-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 0 0 2.25-2.25v-6.75a2.25 2.25 0 0 0-2.25-2.25H6.75a2.25 2.25 0 0 0-2.25 2.25v6.75a2.25 2.25 0 0 0 2.25 2.25Z" />
+      </svg>
+      ` +
+        " Close" +
+        " </a>" +
+        '<a href=""  onclick="deleteIssue(\''+id+'\')" class="flex items-center  gap-1 text-xs text-red-500 bg-white hover:bg-red-50  font-semibold rounded-md  hover:shadow-sm px-2 py-1">' +
+        `     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="w-4 h-4">
+    <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+    </svg>` +
+        " Delete" +
+        "</a> " +
+        "</div>";
 
       (" </div> ");
     }
   }
 }
 
-
-
-// localStorage.clear()
-
-// Close Svg Icon
-/* 
-
- */
-
-// Delete Svg Icon
-/* <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="w-4 h-4">
-<path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
-</svg> */
-
-// localStorage.clear()
+/* Create function to setStatusClosed when the close button is clicked*/
 function setStatusClosed(id) {
+  // get issues from local storage
+  let issues = JSON.parse(localStorage.getItem("issues"));
+
+  // Loop through and match issues id to get clicked issue
+  for (let i = 0; i < issues.length; i++) {
+    if (issues[i].id == id) {
+      issues[i].status = "Closed";
+    }
+
+    // Now Update our localStorage with the new status code
+    localStorage.setItem("issues", JSON.stringify(issues));
+
+    // Fetch updated version from our local storage
+    fetchIssueList();
+  }
+}
+
+//Create function to handle deleteIssue button when clicked
+function deleteIssue(id) {
   // get issues from local storage
   let issues = JSON.parse(localStorage.getItem("issues"));
 
   for (let i = 0; i < issues.length; i++) {
     if (issues[i].id == id) {
-      issues[i].status = "Closed";
+      issues.pop(issues[i]);
     }
 
     // Set the rest of the issues back to local storage
@@ -228,23 +266,10 @@ function setStatusClosed(id) {
   }
 }
 
-// Delete Status Functionality
-function deleteStatus(id) {
-   // get issues from local storage
-   let issues = JSON.parse(localStorage.getItem("issues"));
+// Delete Svg Icon
+/*  */
 
-   for (let i = 0; i < issues.length; i++) {
-     if (issues[i].id == id) {
-       issues.pop(issues[i])
-     }
- 
-     // Set the rest of the issues back to local storage
-     localStorage.setItem("issues", JSON.stringify(issues));
- 
-     // Fetch back issues from local storage
-     fetchIssueList();
-   }
-}
+// localStorage.clear()
 
 // SHow FIlteredIssues Functionality
 
