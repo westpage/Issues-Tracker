cd90c047eebaec079ecc2f92c7d06e8f6dc3bc34
westpage <westpage8@gmail.com>
2024-05-21 17:15:13 -0700

    Feat: Added Functionalities to element

--- public/app.js
+++ public/app.js
@@ -155,264 +155,165 @@ function fetchIssueList() {
 
       //  Now add each issues to our issuesContainer by first clothing each issue element in its case element
 
-      issuesContainer.innerHTML += `
-      
-      <div class="w-full sm:max-w-[550px] md:max-w-[340px] lg:max-w-[420px] mt-6 bg-white shadow-sm hover:shadow-2xl rounded-lg">
-       
-        <div class="w-full flex flex-col bg-white rounded-lg  shadow-md  border border-stone-200 hover:shadow-2xl py-4 px-4">
+      issuesContainer.innerHTML +=
+        ' <div class="w-full sm:max-w-[550px] md:max-w-[340px] lg:max-w-[420px] mt-6 bg-white shadow-sm  hover:shadow-lg rounded-md">' +
+        '<div class="w-full flex flex-col rounded-lg border-dashed border-b-2 border-stone-200 py-4 px-4">' +
+        '      <div class="w-full flex items-center shadow-inner justify-between">' +
+        ' <p class="w-full bg-white flex items-center justify-start py-2 px-2  text-xs text-stone-700 font-bold">Issue ID: ' +
+        ' <span class="bg-white px-2 py-1 rounded-xl shadow-inner text-stone-700 font-medium ml-2">' +
+        id +
+        "</span>" +
+        "    </p>" +
+        '      <p class="w-full bg-white flex items-center justify-end py-2 px-2  text-xs text-stone-700 font-bold">Status: ' +
+        ' <span class="bg-gray-50 shadow-inner text-green-500  font-semibold rounded-xl text-xs ml-2 px-2 py-1">' +
+        status +
+        "  </span>" +
+        "</p>" +
+        "</div>" +
+        '    <p class="w-full bg-white  py-2 px-2  text-xs text-stone-700 font-bold">Assigned To:' +
+        ' <span  class="px-4 text-stone-500 font-semibold">' +
+        assignedTo +
+        " </span>" +
+        " </p>" +
+        ' <p class="w-full  py-2 px-2  text-xs text-stone-700 font-bold">Description:' +
+        '   <span  class="px-4 text-stone-500 leading-5 font-medium">' +
+        description +
+        "  </span>" +
+        "</p>" +
+        '<p class="w-full  py-2 px-2  text-xs text-stone-700 font-bold">Severity:' +
+        '    <span  class="px-4 text-stone-500 font-normal">' +
+        severity +
+        "     </span>" +
+        "  </p>" +
+        "     </div>" +
+        '<div class="w-full px-6 py-4 flex items-center justify-start gap-4">' +
+        '<a href=""  onclick="setStatusClosed(\'' +id+ '\')" class="text-xs text-blue-500 font-semibold rounded-md shadow-sm hover:shadow-xl px-2 py-1">Close </a>' +
+        '<a href=""  onclick="deleteStatus(\'' +id+ '\')" class="text-xs text-red-500 bg-gray-50 font-semibold rounded-md shadow-sm hover:shadow-xl px-2 py-1">Delete </a> ' 
+        +"</div>";
 
-
-
-        <div class="w-full flex items-center justify-between">
-        
-        <p class="w-full bg-white flex items-center justify-start py-2 px-2  text-xs text-stone-700 font-bold">Issue ID: <span class="bg-white px-2 py-1 rounded-xl shadow-inner text-stone-700 font-medium ml-2">${id}</span></p>
-      
-        <p class="w-full bg-white flex items-center justify-end py-2 px-2  text-xs text-stone-700 font-bold">Status: <span class="bg-[#C4DFDF] shadow-inner text-stone-600  font-semibold rounded-xl text-xs ml-2 px-2 py-1">${status}</span></p>
-      </div>
-      
-      <p class="w-full bg-white  py-2 px-2  hover:shadow-inner text-xs text-stone-700 font-bold">Assigned To:
-      <span  class='px-4 text-stone-500 font-semibold'>
-       ${assignedTo}
-        </span></p>
-
-        <p class="w-full  py-2 px-2 hover:shadow-inner text-xs text-stone-700 font-bold">Description:
-        <span  class='px-4 text-stone-500 leading-5 font-medium'>
-         ${description}
-          </span></p>
-
-
-            <p class="w-full  py-2 px-2  hover:shadow-inner text-xs text-stone-700 font-bold">Severity:
-              <span  class='px-4 text-stone-600 font-normal'>
-               ${severity}
-                </span></p>
-
-             
-
-        </div>
-
-        <div class="w-full  px-6 py-4 rounded-sm  flex items-center justify-start space-x-3">
-          <a
-            href=""
-            class="bg-[#C4DFDF] text-xs text-stone-500 font-semibold px-4 py-2 flex items-center gap-1 rounded-sm hover:shadow-2xl"
-          >
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="w-4 h-4">
-          <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 10.5V6.75a4.5 4.5 0 1 1 9 0v3.75M3.75 21.75h10.5a2.25 2.25 0 0 0 2.25-2.25v-6.75a2.25 2.25 0 0 0-2.25-2.25H3.75a2.25 2.25 0 0 0-2.25 2.25v6.75a2.25 2.25 0 0 0 2.25 2.25Z" />
-        </svg>
-        CLOSE
-          </a>
-      
-          <a
-            href=""
-            class="bg-red-50 text-xs text-red-500 px-4 py-2 flex items-center justify-center gap-1 rounded-sm font-semibold hover:shadow-2xl"
-          >
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="w-4 h-4">
-          <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
-        </svg>
-        DELETE
-          </a>
-        </div>
-      </div>
-      
-      
-      `;
+      (" </div> ");
     }
   }
 }
+
+
 
 // localStorage.clear()
 
-// lets get dom elements
-document.addEventListener("DOMContentLoaded", filterIssueList);
+// Close Svg Icon
+/* 
 
-function filterIssueList(e) {
-  // QUICK ACTIONS FUNCTIONALITY
-  var mediumIssues = document.getElementById("mediumIssues");
-  var lowIssues = document.getElementById("lowIssues");
-  var highIssues = document.getElementById("highIssues");
-  var approvedIssues = document.getElementById("approvedIssues");
-  var deletedIssues = document.getElementById("deletedIssues");
+ */
 
-  // View Low Issues in our Catalog
-  lowIssues.addEventListener("click", function (e) {
-    let clicked = e.target.textContent;
+// Delete Svg Icon
+/* <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="w-4 h-4">
+<path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+</svg> */
 
-    console.log(clicked);
-    if (clicked == "Low") {
-      document
-        .getElementById("lowIssues")
-        .classList.add("bg-stone-600", "text-stone-50");
-      document
-        .getElementById("mediumIssues")
-        .classList.remove("bg-stone-600", "text-stone-50");
+// localStorage.clear()
+function setStatusClosed(id) {
+  // get issues from local storage
+  let issues = JSON.parse(localStorage.getItem("issues"));
 
-      document
-        .getElementById("highIssues")
-        .classList.remove("bg-stone-600", "text-stone-50");
-    } else {
-      document
-        .getElementById("lowIssues")
-        .classList.remove("bg-stone-600", "text-stone-50");
-      document
-        .getElementById("mediumIssues")
-        .classList.add("bg-stone-600", "text-stone-50");
-
-      document
-        .getElementById("highIssues")
-        .classList.add("bg-stone-100", "text-stone-700");
+  for (let i = 0; i < issues.length; i++) {
+    if (issues[i].id == id) {
+      issues[i].status = "Closed";
     }
 
-    // get all issues array
-    let issues = JSON.parse(localStorage.getItem("issues"));
+    // Set the rest of the issues back to local storage
+    localStorage.setItem("issues", JSON.stringify(issues));
 
-    // Filter the issues list to get only issues with closed status
-    showFilteredIssues(issues, e, clicked);
-    e.preventDefault();
-  });
+    // Fetch back issues from local storage
+    fetchIssueList();
+  }
 }
 
-// View Medium Issues in our Catalog
-mediumIssues.addEventListener("click", function (e) {
-  let clicked = e.target.textContent;
+// Delete Status Functionality
+function deleteStatus(id) {
+   // get issues from local storage
+   let issues = JSON.parse(localStorage.getItem("issues"));
 
-  console.log(clicked);
-
-  if (clicked == "Medium") {
-    document
-      .getElementById("lowIssues")
-      .classList.remove("bg-stone-600", "text-stone-50");
-
-    document
-      .getElementById("highIssues")
-      .classList.remove("bg-stone-600", "text-stone-50");
-    document
-      .getElementById("mediumIssues")
-      .classList.add("bg-stone-600", "text-stone-50");
-  } else {
-    document
-      .getElementById("mediumIssues")
-      .classList.remove("bg-stone-600", "text-stone-50");
-    document
-      .getElementById("lowIssues")
-      .classList.add("bg-stone-100", "text-stone-700");
-
-    document
-      .getElementById("highIssues")
-      .classList.add("bg-stone-100", "text-stone-700");
-  }
-
-  // get all issues array
-  let issues = JSON.parse(localStorage.getItem("issues"));
-
-  // Filter the issues list to get only issues with closed status
-  showFilteredIssues(issues, e, clicked);
-  e.preventDefault();
-});
-
-// View High Issues in our Catalog
-highIssues.addEventListener("click", function (e) {
-  let clicked = e.target.textContent;
-
-  if (clicked == "High") {
-    document
-      .getElementById("lowIssues")
-      .classList.remove("bg-stone-600", "text-stone-50");
-    document
-      .getElementById("mediumIssues")
-      .classList.remove("bg-stone-600", "text-stone-50");
-
-    document
-      .getElementById("highIssues")
-      .classList.add("bg-stone-600", "text-stone-50");
-  } else {
-    document
-      .getElementById("highIssues")
-      .classList.remove("bg-stone-600", "text-stone-50");
-  }
-
-  console.log(clicked);
-
-  // get all issues array
-  let issues = JSON.parse(localStorage.getItem("issues"));
-
-  // Filter the issues list to get only issues with closed status
-  showFilteredIssues(issues, e, clicked);
-  e.preventDefault();
-});
+   for (let i = 0; i < issues.length; i++) {
+     if (issues[i].id == id) {
+       issues.pop(issues[i])
+     }
+ 
+     // Set the rest of the issues back to local storage
+     localStorage.setItem("issues", JSON.stringify(issues));
+ 
+     // Fetch back issues from local storage
+     fetchIssueList();
+   }
+}
 
 // SHow FIlteredIssues Functionality
 
-function showFilteredIssues(issues, e, clicked) {
-  // Get the issues container
-  var issuesContainer = document.getElementById("issuesContainer");
-  console.log(clicked);
-  // Empty the Issues Container
-  issuesContainer.innerHTML = "";
+// function showFilteredIssues(issues, e, clicked) {
+//   // Get the issues container
+//   var issuesContainer = document.getElementById("issuesContainer");
+//   console.log(clicked);
+//   // Empty the Issues Container
+//   issuesContainer.innerHTML = "";
 
-  if (e.target.textContent === clicked) {
-    for (let i = 0; i < issues.length; i++) {
-      if (issues[i].severity == clicked) {
-        e.target.classList.add("bg-stone-700", "text-stone-50");
-        issuesContainer.innerHTML += `
-     
-        <div class="w-full sm:max-w-[450px] mt-6 bg-white border-dotted shadow-lg border-stone-100 hover:shadow-2xl rounded-xl">
-       
-        <div class="w-full flex flex-col bg-white rounded-xl  shadow-lg border-stone-100 hover:shadow-2xl py-4 px-4">
+//   if (e.target.textContent === clicked) {
+//     for (let i = 0; i < issues.length; i++) {
+//       if (issues[i].severity == clicked) {
+//         e.target.classList.add("bg-stone-700", "text-stone-50");
+//         issuesContainer.innerHTML += `
 
+//         <div class="w-full sm:max-w-[550px] md:max-w-[340px] lg:max-w-[420px] mt-6 bg-white border shadow-md  hover:shadow-2xl rounded-md">
 
+//         <div class="w-full flex flex-col bg-white rounded-lg border-dashed border-b-2 border-stone-200 hover:shadow-2xl py-4 px-4">
 
-        <div class="w-full flex items-center justify-between">
-        
-        <p class="w-full bg-white flex items-center justify-start py-2 px-2  text-xs text-stone-700 font-bold">Issue ID: <span class="bg-white px-2 py-1 rounded-xl shadow text-stone-700 font-semibold ml-2">${id}</span></p>
-      
-        <p class="w-full bg-white flex items-center justify-end py-2 px-2  text-xs text-stone-700 font-bold">Status: <span class="bg-white shadow text-[#]  font-bold rounded-xl text-xs ml-2 px-2 py-1">${status}</span></p>
-      </div>
-      
-      <p class="w-full bg-white  py-2 px-2  hover:shadow-inner text-xs text-stone-700 font-bold">Assigned To:
-      <span  class='px-4 text-[#] font-bold'>
-       ${assignedTo}
-        </span></p>
+//         <div class="w-full flex items-center justify-between">
 
-        <p class="w-full  py-2 px-2 hover:shadow-inner text-xs text-stone-700 font-bold">Description:
-        <span  class='px-4 text-stone-500 font-normal'>
-         ${description}
-          </span></p>
+//         <p class="w-full bg-white flex items-center justify-start py-2 px-2  text-xs text-stone-700 font-bold">Issue ID: <span class="bg-white px-2 py-1 rounded-xl shadow-inner text-stone-700 font-medium ml-2">${id}</span></p>
 
+//         <p class="w-full bg-white flex items-center justify-end py-2 px-2  text-xs text-stone-700 font-bold">Status: <span class="bg-gray-50 shadow-xl text-green-500  font-semibold rounded-xl text-xs ml-2 px-2 py-1">${status}</span></p>
+//       </div>
 
-            <p class="w-full  py-2 px-2  hover:shadow-inner text-xs text-stone-700 font-bold">Severity:
-              <span  class='px-4 text-stone-600 font-normal'>
-               ${severity}
-                </span></p>
+//       <p class="w-full bg-white  py-2 px-2  text-xs text-stone-700 font-bold">Assigned To:
+//       <span  class='px-4 text-stone-500 font-semibold'>
+//        ${assignedTo}
+//         </span></p>
 
-             
+//         <p class="w-full  py-2 px-2 hover:shadow-inner text-xs text-stone-700 font-bold">Description:
+//         <span  class='px-4 text-stone-400 leading-5 font-medium'>
+//          ${description}
+//           </span></p>
 
-        </div>
+//             <p class="w-full  py-2 px-2  text-xs text-stone-700 font-bold">Severity:
+//               <span  class='px-4 text-stone-600 font-normal'>
+//                ${severity}
+//                 </span></p>
 
-        <div class="w-full  px-6 py-4 rounded-sm  flex items-center justify-start space-x-3">
-          <a
-            href=""
-            class="bg-[#] text-xs text-white px-4 py-2 flex items-center gap-1 rounded-md font-semibold hover:shadow-2xl"
-          >
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="w-4 h-4">
-          <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 10.5V6.75a4.5 4.5 0 1 1 9 0v3.75M3.75 21.75h10.5a2.25 2.25 0 0 0 2.25-2.25v-6.75a2.25 2.25 0 0 0-2.25-2.25H3.75a2.25 2.25 0 0 0-2.25 2.25v6.75a2.25 2.25 0 0 0 2.25 2.25Z" />
-        </svg>
-        CLOSE
-          </a>
-      
-          <a
-            href=""
-            class="bg-red-500 text-xs text-white px-4 py-2 flex items-center justify-center gap-1 rounded-md font-semibold hover:shadow-2xl"
-          >
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="w-4 h-4">
-          <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
-        </svg>
-        DELETE
-          </a>
-        </div>
-      </div>
-    
-    
-    `;
-      }
-    }
-  }
-}
+//         </div>
+
+//         <div class="w-full  px-6 py-4 pb-4 rounded-sm  flex items-center justify-start space-x-3">
+//           <a
+//             href=""
+//             class="bg-[#C4DFDF] hover:bg-teal-500 text-xs text-stone-500 font-semibold px-4 py-2 flex items-center gap-1 rounded-sm hover:shadow-2xl"
+//           >
+//           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="w-4 h-4">
+//           <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 10.5V6.75a4.5 4.5 0 1 1 9 0v3.75M3.75 21.75h10.5a2.25 2.25 0 0 0 2.25-2.25v-6.75a2.25 2.25 0 0 0-2.25-2.25H3.75a2.25 2.25 0 0 0-2.25 2.25v6.75a2.25 2.25 0 0 0 2.25 2.25Z" />
+//         </svg>
+//         CLOSE
+//           </a>
+
+//           <a
+//             href=""
+//             class="bg-red-50 hover:bg-red-500 text-xs text-red-500 px-4 py-2 flex items-center justify-center gap-1 rounded-sm font-semibold hover:shadow-2xl"
+//           >
+//           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="w-4 h-4">
+//           <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+//         </svg>
+//         DELETE
+//           </a>
+//         </div>
+//       </div>
+
+//     `;
+//       }
+//     }
+//   }
+// }
